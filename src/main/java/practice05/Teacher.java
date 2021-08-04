da73bb4e5ac7fb17057901279aed49a13d09743e
TORIBDA <david.moses.toribio@oocl.com>
2021-08-04 16:37:46 +0800

    TORIBDA feat: completed practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,25 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return klass!=0?
+                (super.introduce()+ " I am a Teacher. I teach Class " + getKlass() +"."):
+                (super.introduce()+ " I am a Teacher. I teach No Class.");
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
