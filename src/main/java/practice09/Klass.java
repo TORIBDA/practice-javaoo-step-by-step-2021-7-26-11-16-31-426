21da854337e1cb135b48f00455561016d7308009
TORIBDA <david.moses.toribio@oocl.com>
2021-08-05 00:10:15 +0800

    TORIBDA feat: partially completed practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,56 @@
 package practice09;
 
+import practice08.Person;
+
+import java.text.MessageFormat;
+import java.util.LinkedList;
+
 public class Klass {
+    private int num;
+    private Student leader;
+    private LinkedList<Student> members;
+
+    public Klass(int num) {
+        this.num = num;
+        members = new LinkedList<Student>();
+    }
+
+    public int getNumber() {
+        return num;
+    }
+
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", num);
+    }
+
+    public void assignLeader(Student leader) {
+        if(leader.getKlass().getNumber()==this.num) {
+            this.leader = leader;
+        }
+        else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student member) {
+        if(member.getKlass().getNumber()==this.num)
+        {
+            members.add(member);
+        }
+    }
+
+    public boolean isIn(Student student)
+    {
+        System.out.println("Student Class: " + student.getKlass().getNumber());
+        System.out.println("Is a Member: " +  members.contains(student));
+        System.out.print("Members: ");
+        members.forEach(member-> System.out.println(member.getKlass().getNumber()));
+        System.out.println();
+        System.out.println();
+        return members.contains(student);
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,53 @@
 package practice09;
 
-public class Teacher {
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        return klass!=null?
+                MessageFormat.format("{0} I am a Teacher. I teach Class {1}.",
+                        super.introduce(),
+                        klass
+                                .stream()
+                                .map(klassValue-> String.valueOf(klassValue.getNumber()))
+                                .collect(Collectors.joining(", "))):
+                (super.introduce()+ " I am a Teacher. I teach No Class.");
+    }
+
+    public String introduceWith(Student student) {
+        return MessageFormat.format("{0} I am a Teacher. I {1}{2}.", super.introduce(),
+                (isTeaching(student)?"teach ":"don't teach "),
+                student.getName());
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klass;
+    }
+
+    public boolean isTeaching(Student student) {
+        return klass.stream().anyMatch(klassValue ->klassValue.getNumber()==student.getKlass().getNumber());
+//        for (Klass klassValue : klass) {
+//            if(student.getKlass().getNumber()==klassValue.getNumber())
+//            {
+//                System.out.println("Current Klass: " + klassValue.getNumber());
+//                System.out.println("Teacher is teaching the student");
+//                return klassValue.isIn(student);
+//            }
+//        }
+        //return klass.stream().filter(klassValue -> klassValue.isIn(student)).count() > 0;
+    }
 }
