5d2b641a484e55e181267be5644a222439fcf717
TORIBDA <david.moses.toribio@oocl.com>
2021-08-04 22:07:15 +0800

    TORIBDA feat: partially completed practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,45 @@
 package practice08;
 
+
+import java.text.MessageFormat;
+import java.util.LinkedList;
+
 public class Klass {
+    private int num;
+    private Student leader;
+    private LinkedList<Student> members;
+
+    public Klass(int num) {
+        this.num = num;
+        members = new LinkedList<Student>();
+    }
+
+    public int getNumber() {
+        return num;
+    }
+
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", num);
+    }
+
+    public void assignLeader(Student leader) {
+        if(leader.getKlass().getNumber()==this.num) {
+            this.leader = leader;
+        }
+        else {
+            //leader would be null here which would result to failed case
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student member) {
+        if(member.getKlass().getNumber()==this.num)
+        {
+            members.add(member);
+        }
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,34 @@
 package practice08;
 
 public class Person {
+    private final String name;
+    private final int age;
+    private final int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String introduce() {
+        return "My name is " + name +". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object objectValue) {
+        return objectValue instanceof Person && ((Person) objectValue).getId() == this.id;
+    }
 }
